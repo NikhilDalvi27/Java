29b50335f2753bb024adb56d33d77db3df9ebae8
nikhildalvi <nikhildalvi707@gmail.com>
2022-08-28 18:54:58 +0530

    Need for CompletableFutureExample

--- src/Concurrency/CompletableFutureExample.java
+++ src/Concurrency/CompletableFutureExample.java
@@ -1,5 +1,7 @@
 package Concurrency;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.*;
 
@@ -10,59 +12,84 @@ public class CompletableFutureExample {
      */
 
     public static void main(String[] args) {
-//        NeedForCompletableFuture1();
-        NeedForCompletableFuture2();
+        needForCompletableFuture();
+        usingCompletableFuture();
     }
 
-    private static void NeedForCompletableFuture2() {
+    private static void usingCompletableFuture() {
+        long start = System.currentTimeMillis();
+
+        List<CompletableFuture> futureList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            int tempI = i;
+            futureList.add(CompletableFuture.supplyAsync(() -> getOrderTask())
+                    .thenApplyAsync((randomValue) -> getOrderTask2(randomValue))
+                    .thenAccept((randomValue) -> getOrderTaskDependent(tempI, randomValue)));
+        }
+
+        futureList.forEach(CompletableFuture::join);
+        long end = System.currentTimeMillis();
+        System.out.println("Time taken with Completable Future " + (end - start));
+    }
+
+    private static void needForCompletableFuture() {
         Executor service = Executors.newFixedThreadPool(10);
+
+        /** We use future (as a placeholder) to get any returned value from the asynchronous task
+         *  Here we get the placeholder (Future) IMMEDIATELY, Hence the below code line is NON BLOCKING
+         * **/
+        long start = System.currentTimeMillis();
+        ArrayList<Future<Integer>> futures = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            try {
+                // todo Note this if we call get() on future, it is a blocking operation
+                //  till the time value is not available in the future variable
+                //  it blocks the main thread, Problem will amplify if this get() operation is done in a for loop wherein
+                Future<Integer> future = ((ExecutorService) service).submit(new Task());
+                Integer result = future.get();
+
+                // todo below operation is independent of the above result
+                //  Note didn't find an alternative to run dependent task without CompletableFuture
+                future = ((ExecutorService) service).submit(new Task1());
+                result = future.get();
+                System.out.println(i + " recieved Value " + result);
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("Time taken without Completable Future " + (end - start));
+    }
+
+    public static int getOrderTask() {
         try {
-            Future<Integer> future =  ((ExecutorService) service).submit(getOrderTask());
-            Integer res = future.get();
-
-            ((ExecutorService) service).submit(getOrderTaskDependent(res));
-
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
         }
+        return new Random().nextInt();
+
     }
 
-    private static void NeedForCompletableFuture1() {
-        Executor service = Executors.newFixedThreadPool(10);
-
-        /** We use future (as a placeholder) to get any returned value from the asynchronous task **/
-        Future<Integer> future = ((ExecutorService) service).submit(new Task());
-
-        try{
-            // todo Note this is blocking if we call the get() operation
-            //  before the result is available in the future variable
-            // blocks the main thread, Problem will amplify if this get() operation is done in a for loop
-            Integer result = future.get();
-            System.out.println("Result from the task: "+ result);
-        }catch (Exception e){
-            e.printStackTrace();
+    public static int getOrderTask2(int value) {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
         }
+        return value / 10;
+
     }
 
-    public static Callable<Integer> getOrderTask(){
-        return new Callable<Integer>() {
-            @Override
-            public Integer call() throws Exception {
-                int res = new Random().nextInt();
-                System.out.println("Expected value "+ res);
-                return res;
-            }
-        };
-    }
-
-    public static Callable<Integer> getOrderTaskDependent(int prevVal){
-        return new Callable<Integer>() {
-            @Override
-            public Integer call() throws Exception {
-                System.out.println("Received value "+ prevVal);
-                return prevVal;
-            }
-        };
+    public static void getOrderTaskDependent(int tempI, int randomValue) {
+        System.out.println(tempI + " recieved Value " + randomValue);
     }
 
 
@@ -70,6 +97,24 @@ public class CompletableFutureExample {
 
         @Override
         public Integer call() throws Exception {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return new Random().nextInt();
+        }
+    }
+
+    static class Task1 implements Callable<Integer> {
+
+        @Override
+        public Integer call() throws Exception {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
             return new Random().nextInt();
         }
     }
